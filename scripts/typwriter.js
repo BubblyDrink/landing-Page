caf907cfac5516992baec5da3cc275325e9f03e8
BubblyDrink <hero68o@yahoo.com>
2022-09-21 00:46:44 -0500

    Changed layout of page. Added JS scripts

---
+++ scripts/typwriter.js
@@ -0,0 +1,47 @@
+const textDisplay = document.getElementById('text')
+const phrases = ['Im a frontend developer.', 'Backend developer.', 'Overall problem solver.']
+let i = 0
+let j = 0 
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop () {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join('')
+
+  if (i < phrases.length) {
+
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j])
+      j++
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if(isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j])
+      j--
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i === phrases.length) {
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 -50) + 50
+  const normalSpeed = Math.random() * (300 -200) + 3 // change this value to adjust typing speed //
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time)
+}
+
+loop()
